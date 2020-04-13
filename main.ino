1e2d307fc1fe7c52446b38607206d44496a3b0bb
Alvaro Castillo <6666alvaro666@gmail.com>
2020-04-13 16:50:51 -0500

    create a method for move motors

--- main.ino
+++ main.ino
@@ -33,3 +33,14 @@ void setup() {
   v2=250;
  delay(2500);
 }
+//metodos de movimiento
+  //1010
+  void Avanzar (){digitalWrite(in1,HIGH);digitalWrite(in2,LOW);analogWrite(initM1,v1);digitalWrite(in3,HIGH);digitalWrite(in4,LOW);analogWrite(initM2,v2);delay(100);}
+  //1010
+  void Atras   (){digitalWrite(in2,HIGH);digitalWrite(in1,LOW);analogWrite(initM1,v1);digitalWrite(in4,HIGH);digitalWrite(in3,LOW);analogWrite(initM2,v2);delay(100);}
+  //1000
+  void GirarDer(){digitalWrite(in1,HIGH);digitalWrite(in2,LOW);analogWrite(initM1,v1);digitalWrite(in3,LOW);digitalWrite(in4,LOW);analogWrite(initM2,v2);delay(100);}
+  //0010   
+  void GirarIzq(){digitalWrite(in1,LOW);digitalWrite(in2,LOW);analogWrite(initM1,v1);digitalWrite(in3,HIGH);digitalWrite(in4,LOW);analogWrite(initM2,v2);delay(100);}
+  //1111  
+  void Alto    (){digitalWrite(in1,HIGH);digitalWrite(in2,HIGH);analogWrite(initM1,0);digitalWrite(in3,HIGH);digitalWrite(in4,HIGH);analogWrite(initM2,v2);delay(100);}
